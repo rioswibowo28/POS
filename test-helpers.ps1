58a0fed65a885ff18158b9e2a19a691cb43bb4a5
Lipamitra Nusa <lipamitranusa@gmail.com>
2026-03-17 16:43:27 +0700

    chore: remove unused root-level test/dev helper files

--- test-helpers.ps1
+++
@@ -1,9 +0,0 @@
-# Test License Config Helper
-Write-Host "=== Testing License Config Helper ===" -ForegroundColor Cyan
-Write-Host ""
-
-php test-helpers.php
-
-Write-Host ""
-Write-Host "=== Press any key to exit ===" -ForegroundColor Yellow
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
